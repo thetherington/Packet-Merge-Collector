8b421c135d5179a3d688b01bb397c95f90c6e8a7
thetherington <thomas@hetheringtons.org>
2021-03-02 10:43:19 -0500

    first commit

---
+++ scripts/packet_merge.py
@@ -0,0 +1,17 @@
+import copy
+import json
+
+import requests
+
+
+class PacketMergeCollector:
+    def __init__(self, **kwargs):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
